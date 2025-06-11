cb797c7ec17d733998b2f587921dc2fc4280accd
김광수/MD DX팀(P) <kskim01@gsretail.com>
2025-06-11 16:35:40 +0900

    fix: update build configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/vacab-quiz/',
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 })
